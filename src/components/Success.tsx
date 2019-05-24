b08a20a79a9ba9545195cdd8e50c04a1056ba1e6
sylvestertarnowski <34459277+sylvestertarnowski@users.noreply.github.com>
2019-05-24 20:33:13 +0200

    Fix success screen width

--- src/components/Success.tsx
+++ src/components/Success.tsx
@@ -3,9 +3,13 @@ import '../css/Success.css';
 
 const Success: React.FC = () => {
     return (
-        <div className="success">
-            <div className="success-title">Success</div>
-            <div className="success-message">Event has been created.</div>
+        <div className="success-wrapper">
+            <div className="main-width-wrapper">
+                <div className="success">
+                    <div className="success-title">Success</div>
+                    <div className="success-message">Event has been created.</div>
+                </div>
+            </div>
         </div>
     )
 }
